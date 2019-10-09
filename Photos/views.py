282a826401a3b1ee8e1d7f54f99ad1cb2f8847e3
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-10-09 11:19:14 +0200

    displayed photos of today

--- Photos/views.py
+++ Photos/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Persoanl Gallery where you can view different photos ')
+
+def Photos_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+def Photos_of_day(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
