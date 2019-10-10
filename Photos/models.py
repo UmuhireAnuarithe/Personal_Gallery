9670acee920b9fd903f6519e24c70ada02ee9cc5
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-10-10 11:13:41 +0200

    added ralationships

--- Photos/models.py
+++ Photos/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
+class Location(models.Model):
+    lacation = models.CharField(max_length =60)
+
+
+class Category(models.Model):
+    lacation = models.CharField(max_length =60)
+    
+    
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length =30)
     description = models.TextField()
+    location = models.ForeignKey(Location,null=True)
+    category = models.ForeignKey(Category,null= True)
     # pub_date = models.DateTimeField(auto_now_add=True)
     # location = models.ForeignKey(Location)
     
@@ -13,8 +24,3 @@ class Image(models.Model):
     def search_by_name(cls,search_term):
         Photos = cls.objects.filter(name__icontains=search_term)
         return Photos
-
-
-class Location(models.Model):
-    lacation = models.CharField(max_length =60)
-    
