b0320387707ad7f186c05156381a97dd4c3c8646
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-10-10 13:09:14 +0200

    added git ignore

--- Photos/views.py
+++ Photos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
-from .models import Image , Location
+from .models import Image , Location ,Category
 
 # Create your views here.
 
