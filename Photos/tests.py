4591a51161a23049f05d9f5b7a516b81e1f49621
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-10-11 09:30:44 +0200

    done location test

--- Photos/tests.py
+++ Photos/tests.py
@@ -50,17 +50,17 @@ class LocationTestClass(TestCase):
         locations =Location.objects.all()
         self.assertTrue(len(locations)>0)
     
-    # def test_update_image(self):
-    #     self.car = Image(image='travel.jepg',name='Toyota',description='made in Japan')
-    #     self.car.save_image()
-    #     cars =Image.objects.filter(name='Toyota').first()
-    #     update= Image.objects.filter(id=cars.id).update(name='Ritico')
-    #     updated = Image.objects.filter(name = 'Ritico').first()
-    #     self.assertNotEqual(cars.name , updated.name)
+    def test_update_location(self):
+        self.burera = Location(location='Burera')
+        self.burera.save_location()
+        cars =Location.objects.filter(location='Burera').first()
+        update= Location.objects.filter(id=cars.id).update(location='Kinoni')
+        updated = Location.objects.filter(location='Kinoni').first()
+        self.assertNotEqual(cars.location , updated.location)
 
-    # def test_delete_image(self):
-    #     self.cat = Image(image='travel.jepg',name='Vox',description='made in Japan')
-    #     self.cat.save_image()
-    #     cat = Image.objects.filter(name='Vox').first()
-    #     catts = Image.objects.filter(id =cat.id).delete()
-    #     cats =Image.objects.all()
+    def test_delete_location(self):
+        self.burera = Location(location='Burera')
+        self.burera.save_location()
+        location = Location.objects.filter(location='Burera').first()
+        locations = Location.objects.filter(id =location.id).delete()
+        locations =Location.objects.all()
